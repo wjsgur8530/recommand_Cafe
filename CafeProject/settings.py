8fe7ccdc8ccfa7a50567a348070c187501196345
wjsgur8530 <wjsgur8530@gmail.com>
2021-08-11 16:59:23 +0900

    cafe

--- CafeProject/settings.py
+++ CafeProject/settings.py
@@ -151,6 +151,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
