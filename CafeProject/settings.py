bb545c4b3b95adf1a0b203d0b590baa7f8f46991
wjsgur8530 <wjsgur8530@gmail.com>
2021-08-11 19:56:41 +0900

    mysql

--- CafeProject/settings.py
+++ CafeProject/settings.py
@@ -112,35 +112,6 @@ WSGI_APPLICATION = 'CafeProject.wsgi.application'
 #      }
 # }
 
-urlparse.uses_netloc.append('mysql')
-
-try:
-
-    # Check to make sure DATABASES is set in settings.py file.
-    # If not default to {}
-
-    if 'DATABASES' not in locals():
-        DATABASES = {}
-
-    if 'DATABASE_URL' in os.environ:
-        url = urlparse.urlparse(os.environ['DATABASE_URL'])
-
-        # Ensure default database exists.
-        DATABASES['default'] = DATABASES.get('default', {})
-
-        # Update with environment configuration.
-        DATABASES['default'].update({
-            'NAME': url.path[1:],
-            'USER': url.username,
-            'PASSWORD': url.password,
-            'HOST': url.hostname,
-            'PORT': url.port,
-        })
-
-
-        if url.scheme == 'mysql':
-            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
-except Exception:
 
 DATABASES['default'] = {
     'ENGINE': 'django.db.backends.mysql',
