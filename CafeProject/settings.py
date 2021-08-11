fa718862fdfc394afc2043f5e0e5a59e95e37ddf
wjsgur8530 <wjsgur8530@gmail.com>
2021-08-11 17:01:34 +0900

    cafe

--- CafeProject/settings.py
+++ CafeProject/settings.py
@@ -148,9 +148,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
